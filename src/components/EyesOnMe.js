f939909fe501c1d3bc74c2bdd4bece297c62aa35
Robinson Idagiza <robinadoro@gmail.com>
2022-05-09 12:26:41 +0300

    commit tree 8f9b487fb9bc09b7170f621317e0eb9729216898

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+    return(
+        <button
+        onFocus={(e)=>{
+            console.log("Good!");
+        }}
+        onBlur = {()=>{
+            console.log("Hey! Eyes on me!");
+        }}
+        > Eyes on me</button>
+    )
+}
+export default EyesOnMe;
