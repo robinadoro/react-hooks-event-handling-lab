9e311bf516ef970aa68a7d945e47abf4c9b09b25
Robinson Idagiza <robinadoro@gmail.com>
2022-05-09 12:28:07 +0300

    ft:Submit answers

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,16 @@
 // Code Keypad Component Here
+function Keypad(){
+function keypadChange(){
+    console.log("Entering password...");
+}
+
+    return(
+        <div>
+            <input type="password"
+            // name="numb" 
+            onChange={keypadChange}
+            />;
+        </div>
+    )
+}
+export default Keypad;
